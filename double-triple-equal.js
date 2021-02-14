e8a9c210523197b2e1d670679ff22d32b7b64cbc
rakib114 <mahmud17209@gmail.com>
2021-02-14 22:54:24 +0600

    double equal VS triple equal compeare

---
+++ double-triple-equal.js
@@ -0,0 +1,9 @@
+//Double Equal(==) > it's just check variable value
+//Triple Equal (===) > it's check varable value and type
+const first = 2;
+const second = "2";
+if (first === second) {
+  console.log("The condition is truth");
+} else {
+  console.log("The condition is false");
+}
