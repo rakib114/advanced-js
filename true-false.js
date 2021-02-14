0f5f8bb38853fed34e2eba15b81a07a1ec45bacd
rakib114 <mahmud17209@gmail.com>
2021-02-14 15:13:43 +0600

    true vs false Interview:Q

---
+++ true-false.js
@@ -0,0 +1,19 @@
+/**
+  *{False Value >>>}
+  intizer number = 0 >>false
+  empty string = "" >> false
+  undefined >>> false
+  null >> false 
+  NaN >> false
+
+  * {True Values>>>}
+  any value in string = "0", " " >> true
+  empty array = [] >> true
+  empty object = {} >> true
+ */
+let vale = 0;
+if (vale) {
+  console.log("condition is true");
+} else {
+  console.log("condition is false");
+}
